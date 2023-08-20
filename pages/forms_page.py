3e0198ebe963ebc1aa7053f4f513b1036ef4bac2
FrikoGad <goliaf551@mail.ru>
2023-08-20 16:57:16 +0300

    update: -forms_test

--- pages/forms_page.py
+++ pages/forms_page.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 from selenium.webdriver import Keys
 
@@ -25,12 +26,19 @@ class FormsPage(BasePage):
         self.element_is_present(self.locators.FILE_INPUT).send_keys(path)
         os.remove(path)
         self.element_is_visible(self.locators.CURRENT_ADDRESS).send_keys(person.current_address)
-        self.go_to_element(self.element_is_present(self.locators.SELECT_STATE))
         self.element_is_visible(self.locators.SELECT_STATE).click()
         self.element_is_visible(self.locators.STATE_INPUT).send_keys(Keys.RETURN)
         self.element_is_visible(self.locators.SELECT_CITY).click()
         self.element_is_visible(self.locators.CITY_INPUT).send_keys(Keys.RETURN)
         self.element_is_visible(self.locators.SUBMIT).click()
         return person
+
+    def form_result(self):
+        result_list = self.elements_are_present(self.locators.RESULT_TABLE)
+        data = []
+        for item in result_list:
+            self.go_to_element(item)
+            data.append(item.text)
+        return data
 
 
--- tests/forms_test.py
+++ tests/forms_test.py
@@ -6,4 +6,6 @@ class TestForms:
         def test_forms(self, driver):
             form_page = FormsPage(driver, 'https://demoqa.com/automation-practice-form')
             form_page.open()
-            form_page.fill_forms_fields()
+            person_info = form_page.fill_forms_fields()
+            result = form_page.form_result()
+            assert [person_info.first_name + " " + person_info.last_name, person_info.email] == [result[0], result[1]], "the form has not been filled"
